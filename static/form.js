95436579a28125631093a2bffc15066b2e904c5e
jlucena97 <joselucenamorente1997@gmail.com>
2018-11-19 20:12:38 +0100

    commit cambiar before

--- static/form.js
+++ static/form.js
@@ -273,6 +273,7 @@ let set_click_events = () =>{
 	$(document).on('click','.btnAddVariant',function(event){
 		event.preventDefault();
 		let inputValue = $(this).parent().parent().parent().children('input').val();
+		console.log(inputValue);
 		add_new_variant(inputValue);
 	});
 
@@ -778,6 +779,8 @@ let add_new_link = function(title){
  * Insert a new variant for text response
  */
 let add_new_variant = ($btn)=>{
+	$before = $($before_botones + $btn);
+	console.log($before);
 	//$btn.before('<div><input name="response'+numResponses+'" type="text" class="input response validate"><p class="span red-text"></p><ul class="collection"></ul>'
 	//+'<button class="btn-delete-bloq btn btn-primary indigo"><i class="material-icons">delete</i></button></div>');
 	$before_botones.before('<div class="md-form divRespuesta"> <input type="text" class="form-control response validate" placeholder="Respuesta" > <button class="btn btn-sm btn-indigo botonBorrar btn-delete-variant" type="button"><i class="fa fa-trash"></i></button> </div>');
