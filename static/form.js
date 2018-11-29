7a7c50d8376e5fd2006c56d160effacaf1a7207d
jlucena97 <joselucenamorente1997@gmail.com>
2018-11-29 23:21:44 +0100

    commit change cot

--- static/form.js
+++ static/form.js
@@ -348,11 +348,14 @@ let redeclare_input_search = function(){
 	$("#collection").children('.search').remove();
 
 	$inputSearch.keyup(function(e){
-		console.log($(this));
 		if(e.keyCode == 8){
 			if($(this).val().indexOf('#') != -1)
 				$(this).siblings('.span').html('');
-		}
+			} else {
+				$("#collection").hide();
+				$("#collection").children('div').remove();
+				$("#collection").children('.search').remove();
+			}
 	});
 	$inputSearch.unbind('keypress').bind('keypress',function(e){
 		console.log($(this));
