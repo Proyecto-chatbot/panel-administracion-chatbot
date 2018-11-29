cd9638bad617a4ccf1f0d9b4d6438c084eba557f
jlucena97 <joselucenamorente1997@gmail.com>
2018-11-30 00:19:11 +0100

    commit change

--- static/form.js
+++ static/form.js
@@ -658,9 +658,9 @@ let redeclarate_btn_delete_bloq = () =>{
 let redeclarate_btn_delete = () =>{
 	$btnDeleteVariant = $('.btn-delete-variant');
 	$btnDeleteVariant.click(function(event){
-		console.log($(this));
 		event.preventDefault();
 		$(this).parent('div').remove();
+		redeclare_input_search();
 	});
 }
 /**
@@ -678,7 +678,7 @@ let redeclarate_btn_delete_synonym = () =>{
 */
 let add_new_input = ($input)=>{
 	//$input.before('<div><input class="input user validate" name="user" type="text" ><p class="span red-text"></p><ul class="collection"></ul><button class="btn-delete-variant btn btn-primary indigo"><i class="material-icons">delete</i></button></div>');
-	$(".btnPregunta").before('<div class="md-form divPregunta"> <input type="text" class="form-control validate user input" placeholder="Pregunta" name="user"> <button class="btn btn-sm btn-indigo botonBorrar btn-delete-variant" type="button"><i class="fa fa-trash"></i></button></div>');
+	$("#collection").before('<div class="md-form divPregunta"> <input type="text" class="form-control validate user input" placeholder="Pregunta" name="user"> <button class="btn btn-sm btn-indigo botonBorrar btn-delete-variant" type="button"><i class="fa fa-trash"></i></button></div>');
 	redeclarate_btn_delete();
 	redeclare_input_search();
 }
