b1a66193d35b25e54b5fc0a059883f3bd483db51
José Lucena <joselucenamorente@outlook.es>
2019-06-02 19:30:29 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -577,8 +577,8 @@ post_intent = (req,res,next)=>{
 				webhookUsed: false },
 				json: true
 			};
-			res.send(postOptions);
-			request(postOptions, function (error, response, body) {
+			
+		request(postOptions, function (error, response, body) {
 		if (error) throw new Error(error);
 		botMessages = [];
 		res.send(body);
