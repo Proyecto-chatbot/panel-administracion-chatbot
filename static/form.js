1f90dd72162e71427ab1a75ede8277cac1875c32
José Lucena <joselucenamorente@outlook.es>
2019-06-12 23:31:54 +0200

    Add files via upload

--- static/form.js
+++ static/form.js
@@ -311,7 +311,7 @@ let set_click_events = () =>{
 		console.log(token);
 		$.post('/bot',{token : token}, function(response){
 			console.log(response);
-			//location.href = response;
+			location.href = response;
 		});
 	});
 	$btn_add_question.click(function(event){
