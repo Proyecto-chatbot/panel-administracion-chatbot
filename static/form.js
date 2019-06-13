ee427872a1a47551d7b7ae86f723087f1ed0f92d
José Lucena <joselucenamorente@outlook.es>
2019-06-13 21:05:44 +0200

    Add files via upload

--- static/form.js
+++ static/form.js
@@ -321,7 +321,7 @@ let set_click_events = () =>{
 	$btn_show_user.click(function(event){
 		event.preventDefault();
 		console.log('clicked');
-		let user = $(this).val();
+		let user = $(this).html();
 		console.log(user);
 		$.post('/user',{'user' : user}, function(response){
 			console.log(response);
