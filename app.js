72a18910fc63a2338c4dda4fef57bfa1780a016a
José Lucena <joselucenamorente@outlook.es>
2019-06-14 00:48:03 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -906,7 +906,7 @@ app.post('/setuser', function (req, res, next) {
 	let user = req.body.user;
 	let password = req.body.password;
 	service.set_users(user, password);
-	res.send('/validate');
+	res.send('ok');
 });
 
 app.post('/deny', function (req, res, next) {
